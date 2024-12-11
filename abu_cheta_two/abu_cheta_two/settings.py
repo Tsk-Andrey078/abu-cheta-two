261ebdda936731579419b6b4f1c30ab9c9961de8
Yerkow <112828364+yerkow@users.noreply.github.com>
2024-12-11 13:49:11 +0500

    Update settings.py

--- abu_cheta_two/abu_cheta_two/settings.py
+++ abu_cheta_two/abu_cheta_two/settings.py
@@ -102,16 +102,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
 ROOT_URLCONF = 'abu_cheta_two.urls'
 
 TEMPLATES = [
