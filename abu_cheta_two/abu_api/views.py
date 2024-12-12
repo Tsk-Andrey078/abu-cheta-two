e1bae86a758155a066dd4438a503af3d8a8af33c
Tsk-Andrey078 <tskandrey2002.@gmail.com>
2024-12-12 12:06:49 +0500

    Auth

--- abu_cheta_two/abu_api/views.py
+++ abu_cheta_two/abu_api/views.py
@@ -8,11 +8,13 @@ from .serializer import ParticipantSerializer, ColichestvoDebilovSerializer
 from rest_framework.permissions import IsAuthenticated
 
 class ParticipantsViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Participant.objects.all()
     serializer_class = ParticipantSerializer
     lookup_field = "id"
 
 class ColDebViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = ColichestvoDebilov.objects.all()
     serializer_class = ColichestvoDebilovSerializer
     lookup_field = "id"
